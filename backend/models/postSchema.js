09f8bab70b3c6f84a4ebe8859086cbeb79436aad
yashdev2712 <deveshchandra2712@gmail.com>
2024-07-01 02:59:47 +0530

    comments fix

--- backend/models/postSchema.js
+++ backend/models/postSchema.js
@@ -13,10 +13,18 @@ const postSchema = mongoose.Schema({
     }, likes: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: "User"
-    }], comments: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User"
-    }]
+    }], comments: [
+        {
+            text: {
+                type: String,
+                required: true
+            }, user: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "User",
+                required: true
+            }
+        }
+    ]
 }, { timestamps: true });
 
 
