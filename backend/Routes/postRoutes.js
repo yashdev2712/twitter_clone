fcf3184ac16e7e19d390c66040bf0a6a79bb07b2
yashdev2712 <deveshchandra2712@gmail.com>
2024-07-02 06:24:44 +0530

    fetch user post

--- backend/Routes/postRoutes.js
+++ backend/Routes/postRoutes.js
@@ -1,15 +1,16 @@
 import express from "express";
 import middleware from "../middleware/middleware.js";
-import {createPost,deletePost,likeorUnlikePost,commentPost,getAllPost,getLikedPost,followingPost} from "../controller/postController.js";
+import { createPost, deletePost, likeorUnlikePost, commentPost, getAllPost, getUserPosts, getLikedPost, followingPost } from "../controller/postController.js";
 
 
 const router = express.Router();
 
-router.post("/create",middleware,createPost);
-router.delete("/delete/:id",middleware,deletePost);
-router.post("/like/:id",middleware,likeorUnlikePost);
-router.post("/comment/:id",middleware,commentPost);
-router.get("/all",middleware,getAllPost);
-router.get("/getLikedPost/:id",middleware,getLikedPost);
-router.get("/followingPost",middleware,followingPost)
+router.get("/all", middleware, getAllPost);
+router.get("/getLikedPost/:id", middleware, getLikedPost);
+router.get("/followingPost", middleware, followingPost);
+router.get("/user/:username", middleware, getUserPosts);
+router.post("/create", middleware, createPost);
+router.delete("/delete/:id", middleware, deletePost);
+router.post("/like/:id", middleware, likeorUnlikePost);
+router.post("/comment/:id", middleware, commentPost);
 export default router;
--- backend/controller/postController.js
+++ backend/controller/postController.js
@@ -228,3 +228,34 @@ export const followingPost = async (req, res) => {
 
     }
 }
+
+export const getUserPosts = async (req, res) => {
+    try {
+        const { username } = req.params;
+        const user = await User.findOne({ username });
+
+        if (!user) {
+            return res.status(404).json({
+                error: "User not found"
+            })
+        }
+
+        const posts = await Post.find({ user: user._id })
+            .sort({ createdAt: -1 })
+            .populate({
+                path: "user",
+                select: "-password"
+            })
+            .populate({
+                path: "comments.user",
+                select: "-password"
+            })
+
+            return res.status(200).json(posts)
+    }
+    catch (error) {
+        console.log("Error in getLikedPosts controller: ", error);
+        res.status(500).json({ error: "Internal server error" });
+
+    }
+}
